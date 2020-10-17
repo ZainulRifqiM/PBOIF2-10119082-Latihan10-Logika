14495acd1e2c10a0796b2a2d784cc9fb84437305
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 17:02:26 +0700

    PBOIF2-10119082-Latihan10-Logika

---
+++ src/pboif2/pkg10119082/latihan10/logika/PBOIF210119082Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan10.logika;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Logika
+ */
+public class PBOIF210119082Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa :" + dewasa);
+    }
+    
+}
